269760e5ed55d522304cdd5e0467e97363164512
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-03-28 02:13:57 -0400

    h

---
+++ HunterCFreemanSite/Shared/FilterMenuItem.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HunterCFreemanSite.Shared
+{
+    public partial class FilterMenuItem : ComponentBase
+    {
+        [Parameter]
+        public string DisplayText { get; set; }
+    }
+}
