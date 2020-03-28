3625bb2ff0365e375bd83d538247a43afcfaa1fe
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-03-28 02:08:38 -0400

    s

---
+++ HunterCFreemanSite/Shared/FilterMenu.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HunterCFreemanSite.Shared
+{
+    public partial class FilterMenu : ComponentBase
+    {
+        public int Width { get; set; } = 250;
+        public bool IsCollapsed { get; set; }
+        public string Height 
+        {
+            get => IsCollapsed ? "56px" : "100vh";
+        }
+    }
+}
