3feafed69bd88785a50b6745f92212ce1d8a3747
Hunter Freeman <hunter.freeman@darkmatter2bd.com>
2020-04-03 21:07:34 -0400

    a

---
+++ HunterCFreemanSite/Shared/ActiveFilterItem.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HunterCFreemanSite.Shared
+{
+    public partial class ActiveFilterItem : ComponentBase
+    {
+        [Parameter]
+        public string DisplayText { get; set; }
+        [Parameter]
+        public Action RemoveFilter { get; set; }
+    }
+}
